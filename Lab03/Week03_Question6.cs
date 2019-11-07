60b713adc17053e452eeb5bf2dacc4e751786eb7
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-07 15:39:41 -0500

    Add files via upload

---
+++ Lab03/Week03_Question6.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Question 6
+            int counter = 60;
+            while (counter > 19)
+            {
+                Console.WriteLine(counter);
+                counter = counter - 1;
+            }
+        }
+    }
+}

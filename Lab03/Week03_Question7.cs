676d3e79d916cec104f63960b366dea6caa5a0d0
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-07 15:41:37 -0500

    Add files via upload

---
+++ Lab03/Week03_Question7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Question 7
+            int counter = 10;
+            while (counter < 21)
+            {
+                Console.WriteLine(counter);
+                counter = counter + 1;
+            }
+        }
+    }
+}

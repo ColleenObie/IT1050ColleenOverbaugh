4eb2bec84cb360f93935ffd4ea1e5f8db862d12c
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-14 19:45:20 -0500

    Add files via upload

---
+++ Lab04/Week04_Question7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Question 7 (Needs to increment by 1)
+        {
+            for (int i = 0; i < 101; i++)
+
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("********");
+            }
+        }
+    }
+}

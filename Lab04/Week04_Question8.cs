f3b93c1957ebea107b05a1a5eb088b713783840a
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-14 19:45:51 -0500

    Add files via upload

---
+++ Lab04/Week04_Question8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Question 8
+        {
+            for (int i = 0; i < 101; i++)
+
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("********");
+            }
+           
+        }
+    }
+}

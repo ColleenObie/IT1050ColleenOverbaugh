f03ea07f471d9d2e1cf63e9022db6c3772669a11
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-17 17:44:37 -0500

    Add files via upload

---
+++ Lab04/A-B_ExtraCredit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04_ExtraCredit
+{
+    class Program
+    { 
+        static void Main(string[] args)
+       
+        {   
+            //Extra Credit Triangle A and B.
+            int i;
+            int j;
+
+            for (i = 0; i < 10; i++)
+            {
+                for (i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("**********".Substring(0, i));
+                }
+                Console.WriteLine();
+            }
+            for (j = 10; j > 0; j--)
+            {
+                for (j = 10; j > 0; j--)
+                {
+                    Console.WriteLine("**********".Substring(0, j));
+                }
+                Console.WriteLine();
+            }
+
+
+        }  
+
+    }
+}

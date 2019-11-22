4a13fe1b5b3ca75f333404d40189824ae5214918
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-22 18:51:11 -0500

    Add files via upload

---
+++ Lab05/DogClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+        public class Dog
+        {
+            public string Name;
+            public static void Main(string[] args)
+            {
+                Dog myDog = new Dog();
+                myDog.Name = "Fido";
+
+                myDog.bark();
+                myDog.doTrick("Roll");
+            }
+
+            private void bark()
+            {
+                Console.WriteLine("{0} is barking... ", Name);
+            }
+
+            private void doTrick(string trickName)
+            {
+                Console.WriteLine("{0} is so smart! {0} is doing a(n) {1}", Name, trickName);
+            }
+        }
+}
+
+

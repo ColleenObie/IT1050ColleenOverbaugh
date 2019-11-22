009a52ae84a057ac73378fb2f5478ffeec00b9db
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-21 20:01:32 -0500

    Add files via upload

---
+++ Midterm/Classes and Objects Persons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Objects
+{
+    class Sweater
+    {
+        string color = "purple";
+        static void Main(string[] args)
+        {
+            Sweater mySweater = new Sweater();
+            Console.WriteLine(mySweater.color);
+        }
+    }
+}

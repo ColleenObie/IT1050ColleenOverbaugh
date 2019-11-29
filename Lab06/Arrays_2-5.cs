62cda3977989c1af0a0b9c4af89f4571c782365c
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-28 20:48:54 -0500

    Add files via upload

---
+++ Lab06/Arrays_2-5.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /* //Question 2
+            int[] months = new int[12];
+                string[] month = new string[12];
+
+                month[0] = "January";
+                month[1] = "February";
+                month[2] = "March";
+                month[3] = "April";
+                month[4] = "May";
+                month[5] = "June";
+                month[6] = "July";
+                month[7] = "August";
+                month[8] = "September";
+                month[9] = "October";
+                month[10] = "November";
+                month[11] = "December";
+
+            for (int counter = 0; counter < month.Length; counter++)
+
+            {
+                Console.Write( counter + 1 + " ");
+                Console.WriteLine( month [counter]);
+            }   
+
+            */
+
+            /*Question 3
+            string[] season = { "Spring", "Summer", "Fall", "Winter" };
+
+            foreach (string seasons in season)
+                Console.WriteLine(seasons);
+            */
+
+            /*Question 4
+            Random random = new Random();
+            int[] randomNumber = new int[1000];
+
+            for (int i = 0; i < randomNumber.Length; i++)
+                randomNumber[i] = random.Next(0, 100);
+ 
+            foreach (int i in randomNumber)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            */
+            /*Question 5
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+            int i = 0;
+
+            for (i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            */
+        }
+    }
+}

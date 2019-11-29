7ff298ce0815b6139eb62b0add4ff2985b0aa012
Colleen Overbaugh <54554704+ColleenObie@users.noreply.github.com>
2019-11-28 22:05:08 -0500

    Add files via upload

---
+++ Lab06/Array_6-7.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*Question 6 
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+            int i = 0;
+            while (i < names.Length)
+            {
+                Console.WriteLine("{0,2}. {1}", i, names[i++]);
+            }
+            */
+            //Question 7
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+            int i = 0;
+            while (i < names.Length)
+            {
+                Console.WriteLine("{0,2}. {1}", i, names[i++]);
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine("{0,2}. {1}", i, names[i++]);
+            }
+        }
+    }
+}
